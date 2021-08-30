b3e21b5d10d524594ac171c4f91aba3b708d6084
Vineet Koppalkar <koppalkarvineet@gmail.com>
2021-08-30 12:09:06 -0400

    Setup angular tutorial project

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent {
+  shippingCosts = this.cartService.getShippingPrices();
+
+  constructor(private cartService: CartService) {}
+}
